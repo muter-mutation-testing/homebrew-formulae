a86f055a55e0a7a5b7c6044f2709ec0aa80edc28
SeanROlszewski <sean.r.olszewski@gmail.com>
2018-12-25 11:32:01 -0500

    Update Muter's formula for its v0.2.0 release

--- Formula/muter.rb
+++ Formula/muter.rb
@@ -1,8 +1,8 @@
 class Muter < Formula
-  desc "Automated mutation testing for Swift code."
+  desc "Automated mutation testing for Swift code"
   homepage "https://github.com/SeanROlszewski/muter/"
-  url "https://github.com/SeanROlszewski/muter/archive/v0.1.0.zip"
-  sha256 "afc75463130310da268af3816c4d24844254f16d73dddfda5335cf961ac762d1"
+  url "https://github.com/SeanROlszewski/muter/archive/v0.2.0.zip"
+  sha256 "a98dccb8f28f44e1d97193fb6551b77036e16be30082e24991a54d6f28fa920f"
 
   def install
     system "make", "install", "prefix=#{prefix}" 
