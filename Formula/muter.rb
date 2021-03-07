037f400e36e34b67b3e664d8ef770958c35e4aa6
Rafael Machado <raka.cdc@gmail.com>
2021-03-07 20:30:39 -0300

    Update Muter's formula for its v15 release

--- Formula/muter.rb
+++ Formula/muter.rb
@@ -1,8 +1,8 @@
 class Muter < Formula
 	desc "Automated mutation testing for Swift"
 	homepage "https://github.com/muter-mutation-testing/muter/"
-	url "https://github.com/muter-mutation-testing/muter/archive/v14.zip"
-	sha256 "352177855f68ac428a34702ee8e94a80434b11822a1ecaffc58cc7c155ad6108"
+	url "https://github.com/muter-mutation-testing/muter/archive/v15.zip"
+	sha256 "8b34a314d6e4c10c0e33d3da34d62c838c36168ff8a41ba41b2fd0294f1b14fc"
 	
 	def install
 		system "make", "install", "prefix=#{prefix}"
